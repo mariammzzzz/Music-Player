818b82d717741a0f7afd42bd6f6962ecb6041102
mjapa21 <mjapa21@freeuni.edu.ge>
2026-06-12 02:57:08 +0400

    Add album cover, title, progress indicator

--- app/src/main/java/com/mjapa21/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/mjapa21/musicplayerapp/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mjapa21.musicplayerapp.ui.theme.MusicPlayerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +38,94 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MusicPlayerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                MusicPlayerScreen(
+                    song = "Touch", artist = "Daft Punk"
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MusicPlayerScreen(song: String, artist: String) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(), topBar = {
+            TopAppBar(title = {
+                Box(
+                    modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+                ) { Text(text = "Now Playing", fontSize = 16.sp) }
+            }, navigationIcon = {
+                IconButton(content = {
+                    Icon(
+                        painter = painterResource(R.drawable.outline_arrow_downward_24),
+                        contentDescription = null
+                    )
+                }, onClick = { /* Handle back navigation */ })
+            }, actions = {
+
+                IconButton(content = {
+                    Icon(
+                        painter = painterResource(R.drawable.three_dots_image),
+                        modifier = Modifier.size(24.dp),
+                        contentDescription = null
+                    )
+                }, onClick = { /* Handle back navigation */ })
+
+
+            })
+        }) { innerPadding ->
+        
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(horizontal = 28.dp)
+                        .clip(RoundedCornerShape(24.dp)),
+                    painter = painterResource(R.drawable.daft_punk_ram),
+                    contentDescription = null
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 34.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(
+                                vertical = 16.dp
+                            ), horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(text = song, fontSize = 24.sp)
+                            Text(text = artist, fontSize = 16.sp, color = Color.Gray)
+                        }
+                        IconButton(content = {
+                            Icon(
+                                painter = painterResource(R.drawable.outline_favorite),
+                                contentDescription = null,
+                                modifier = Modifier.size(24.dp)
+                            )
+                        }, onClick = { /* Handle back navigation */ })
+                    }
+
+                    LinearProgressIndicator(
+                        progress = { 0.5f },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+
                     )
                 }
             }
@@ -30,18 +133,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+private fun MusicPlayerScreenPreview() {
     MusicPlayerAppTheme {
-        Greeting("Android")
+        MusicPlayerScreen("Touch", "Daft Punk")
     }
 }
